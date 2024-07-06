76393a3e4b0192666406f40801af19ce91771f0e
Paras Jain <pparasjain41@gmail.com>
2024-07-06 22:25:04 +0530

    mongodb connect complate

---
+++ src/db/index.db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+// console.log(`Ths is BD`${process.env.MONGODB_URI}),
+
+
+const connectDB = async () => {
+    try {
+        // const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance=  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n  MongoDB  Connected !!  DB Host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB CONNECTION FAILED",error);
+        process.exit(1)
+        
+    }
+}
+
+export default connectDB
